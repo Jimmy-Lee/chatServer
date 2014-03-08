60b9ace3ec38e999110ed5274cb3024f0379ea22
Jimmy-Lee <b00901058@ntu.edu.tw>
2014-03-09 04:43:21 +0800

    更新小功能
    模式變更為要先輸入名稱才能傳訊息
    重複的名稱會被辨識出來
    可以看一下傳訊息的地方，我覺得可以改進

--- chatServer/Properties/AssemblyInfo.cs
+++ chatServer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 組件的一般資訊是由下列的屬性集控制。
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
-[assembly: AssemblyTitle("server")]
+[assembly: AssemblyTitle("chatServer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("server")]
+[assembly: AssemblyProduct("chatServer")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
-[assembly: Guid("2a6a62b1-883d-4141-8a2b-ba914006122e")]
+[assembly: Guid("c73e8457-2a22-49e1-8ccc-9b219b628478")]
 
 // 組件的版本資訊是由下列四項值構成:
 //
---
+++ chatServer/chatServer.cs
@@ -0,0 +1,96 @@
+﻿
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace chatServer
+{
+    class chatServer
+    {
+        List<chatSocket> clientList = new List<chatSocket>();
+        List<String> clientIDList = new List<string>();
+        
+        public static void Main(String[] args)
+        {
+            chatServer server = new chatServer();
+            server.run();
+        }
+
+        public void run()
+        {
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, setting.port);
+            Socket newSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            newSocket.Bind(ipep);
+            newSocket.Listen(100);
+
+            while (true)
+            {
+                Socket socket = newSocket.Accept();
+                Console.WriteLine("-- WAITING FOR CONNECTIONS --\n");
+                chatSocket client = new chatSocket(socket);
+
+                try
+                {
+                    clientList.Add(client);
+                    client.newListener(processMessage);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        public String processMessage(String msg)
+        {
+            Console.WriteLine("RECEIVED：" + msg);
+            if (msg.IndexOf("Welcome: ") == 0)
+            {
+                String ID = msg.Substring(9);
+                if (clientIDList.Contains(ID))
+                {
+                    clientList.Remove(clientList.Last());
+                    return "";
+                }
+                else
+                {
+                    clientIDList.Add(ID);
+                    broadCast(msg);
+                    String command = "IDListUpdate:";
+                    for (int i = 0, L = clientIDList.Count; i < L; ++i)
+                    {
+                        if (i == 0)
+                            command += ' ' + clientIDList[i];
+                        else
+                            command += ':' + clientIDList[i];
+                    }
+                    broadCast(command);
+                    return "";
+                }
+            }
+            String nameAndTime = msg.Substring(0, msg.IndexOf(':') - 1) + " ─ " + DateTime.Now.ToLongTimeString();
+            String message = msg.Substring(msg.IndexOf(':') + 2);
+            broadCast(nameAndTime + "\n→  " + message);
+            return "";
+        }
+
+        public void broadCast(String msg)
+        {
+            Console.WriteLine("BROADCAST：\"" + msg + "\" to " + clientList.Count + " people");
+            foreach (chatSocket client in clientList)
+            {
+                if (client.active)
+                {
+                    Console.WriteLine("  Send to " + client.remoteEndPoint.ToString());
+                    client.sendMessage(msg);
+                }
+            }
+            Console.WriteLine("");
+        }
+    }
+}
